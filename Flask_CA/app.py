d54ac54ca4c9cfdeee188fb29376baed0c922dab
Venkata Sreenivas Prasad Kasibhatla <vn769140@dal.ca>
2024-08-03 20:48:00 -0300

    fix: fix in the frontend

--- Flask_CA/app.py
+++ Flask_CA/app.py
@@ -181,10 +181,8 @@ def upload_file():
         except Exception as e:
             return render_template_string(f'''<div class="container">
                                                 <div class="alert alert-success" role="alert">
-                                                    File "{file.filename}" uploaded successfully. <br>
-                                                    <a href="/download/{transcribed_filename}" class="btn btn-success mt-2">Download Transcribed File</a>
+                                                    File uploaded successfully. <br>
                                                 </div>
-                                                <a href="/" class="btn btn-primary mt-2">Upload Another File</a>
                                             </div>''')
 
 
